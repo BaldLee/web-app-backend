7c39d1482c00357ab4d9a880c13b86ccc704d069
bald-win <1351533120@qq.com>
2019-04-20 22:05:20 +0800

    写了个基于spring jpa的demo，运行不能

--- src/main/java/sjtu/webapplication/ebook/controller/EbookController.java
+++ src/main/java/sjtu/webapplication/ebook/controller/EbookController.java
@@ -1,25 +1,41 @@
 package sjtu.webapplication.ebook.controller;
 
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import sjtu.webapplication.ebook.bookInfo.BookInfoGet;
+import sjtu.webapplication.ebook.repository.BookRepository;
+import sjtu.webapplication.ebook.service.BookService;
+
+import javax.annotation.Resource;
 
 @RestController
 @EnableAutoConfiguration
 public class EbookController {
+
+    @Resource
+    private BookService bookService;
+
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         String output = "/";
         return output;
     }
 
     @CrossOrigin
-    @RequestMapping(value="/books",method = RequestMethod.POST,consumes = "Application/json")
-    public String books(){
-        BookInfoGet tmp = new BookInfoGet();
+    @RequestMapping(value = "/books", method = RequestMethod.POST, consumes = "Application/json")
+    public String books() {
+        BookService tmp = new BookService();
         return tmp.get();
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/books2", method = RequestMethod.POST, consumes = "Application/json")
+    public String books2() {
+        return JSON.toJSONString(bookService.getAll().iterator());
     }
 }
