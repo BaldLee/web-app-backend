f6d04859bacc642e7dd4527fd60fbcb6f8ebf496
bald-win <1351533120@qq.com>
2019-04-20 23:49:33 +0800

    4.20 暂停

--- src/main/java/sjtu/webapplication/ebook/controller/EbookController.java
+++ src/main/java/sjtu/webapplication/ebook/controller/EbookController.java
@@ -1,14 +1,11 @@
 package sjtu.webapplication.ebook.controller;
 
 import com.alibaba.fastjson.JSON;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import sjtu.webapplication.ebook.repository.BookRepository;
 import sjtu.webapplication.ebook.service.BookService;
 
 import javax.annotation.Resource;
--- src/main/java/sjtu/webapplication/ebook/service/BookService.java
+++ src/main/java/sjtu/webapplication/ebook/service/BookService.java
@@ -1,22 +1,10 @@
 package sjtu.webapplication.ebook.service;
 
-import com.alibaba.fastjson.JSON;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.stereotype.Service;
 import sjtu.webapplication.ebook.entity.Book;
 import sjtu.webapplication.ebook.repository.BookRepository;
 
-
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 @Service
 public class BookService {
@@ -28,36 +16,4 @@ public class BookService {
         return bookRepository.findAll();
     }
 
-    public ArrayList<Book> getBookIfo() {
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
-        StandardServiceRegistry registry = builder.configure().build();
-        SessionFactory factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        Session session = factory.openSession();
-        Transaction transaction = null;
-        ArrayList<Book> result = new ArrayList<Book>();
-
-        try {
-            transaction = session.beginTransaction();
-            List books = session.createQuery("FROM books").list();
-            for (Iterator iterator = books.iterator();
-                 iterator.hasNext(); ) {
-                Book book = (Book) iterator.next();
-                result.add(book);
-            }
-            transaction.commit();
-        } catch (HibernateException ex) {
-            if (transaction != null) transaction.rollback();
-            ex.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        return result;
-    }
-
-    public String get() {
-        ArrayList<Book> result = this.getBookIfo();
-        String output = JSON.toJSONString(result);
-        return output;
-    }
 }
