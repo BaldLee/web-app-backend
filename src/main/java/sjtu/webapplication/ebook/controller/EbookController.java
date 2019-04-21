f4d1c36788740de3cdfa45d676807af3cf9f3f70
bald-win <1351533120@qq.com>
2019-04-21 10:59:05 +0800

    清理引入

--- src/main/java/sjtu/webapplication/ebook/controller/EbookController.java
+++ src/main/java/sjtu/webapplication/ebook/controller/EbookController.java
@@ -2,14 +2,12 @@ package sjtu.webapplication.ebook.controller;
 
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import sjtu.webapplication.ebook.service.BookService;
 
-import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/ebook")
