626d740e50a7730344bab454f3f48b218d46c9e8
bald-win <1351533120@qq.com>
2019-04-22 18:28:43 +0800

    书籍增加

--- src/main/java/sjtu/webapplication/ebook/controller/EbookController.java
+++ src/main/java/sjtu/webapplication/ebook/controller/EbookController.java
@@ -33,7 +33,7 @@ public class EbookController {
     @CrossOrigin
     @RequestMapping(value = "/books/getall", method = RequestMethod.POST, consumes = "application/json")
     public String books() {
-        return JSON.toJSONString(bookService.getAll().iterator());
+        return bookService.getAll();
     }
 
     @CrossOrigin
@@ -54,14 +54,19 @@ public class EbookController {
     @CrossOrigin
     @RequestMapping(value = "/books/update",method = RequestMethod.POST,consumes = "application/json")
     public String booksUpdate(@RequestBody Book book){
-        Book result = bookService.updateBook(book);
-        return JSON.toJSONString(result);
+        return bookService.updateBook(book);
     }
 
     @CrossOrigin
     @RequestMapping(value = "/books/delete",method = RequestMethod.POST,consumes = "application/json")
     public String booksDelete(@RequestBody Book book){
         bookService.deleteBook(book);
-        return "done";
+        return "delete done";
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/books/add",method = RequestMethod.POST,consumes = "application/json")
+    public String booksadd(@RequestBody Book book){
+        return bookService.updateBook(book);
     }
 }
